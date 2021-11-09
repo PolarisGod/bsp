7fb68d32062cf170689fa4d84e5a13ac50e6ee50
wangxiaofei <15690846393@163.com>
2021-11-09 16:02:23 +0800

    bsp初始化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filter/exception.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,5 +10,6 @@ async function bootstrap() {
   // 全局异常处理
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
+  Logger.log('app启动成功!');
 }
 bootstrap();
--- src/middle/log.middle.ts
+++ src/middle/log.middle.ts
@@ -1,3 +1,9 @@
-export class LoggerMiddleWare {
+import { NestMiddleware } from "@nestjs/common";
+import { NextFunction } from "express";
+
+export class LoggerMiddleWare implements NestMiddleware{
+  async use(req: any, res: any, next: NextFunction) {
+    await next();
+  }
   
 }
